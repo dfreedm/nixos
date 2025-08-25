920a0f630b98750ade82c71b6606401e110b36e8
Daniel Freedman <dfreedm2@gmail.com>
2025-08-25 09:54:53 -0700

    Create separate host folders, adjust paths

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     # NixOS official package source, using the nixos-25.05 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    # NixOS Hardware configuration source
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
@@ -19,10 +20,11 @@
       nixosConfigurations.chromedome = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
+          # Import the NixOS Hardware config first, following modules can override
           nixos-hardware.nixosModules.framework-13-7040-amd
           # Import the previous configuration.nix we used,
           # so the old configuration file still takes effect
-          ./configuration.nix
+          ./hosts/chromedome/configuration.nix
         ];
       };
     };
