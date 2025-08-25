3e33f3222718c6ec01590743d62c1c3372fc5ba5
Daniel Freedman <dfreedm2@gmail.com>
2025-08-24 19:39:18 -0700

    Switch nixos-hardware to flake url

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@ description = "Chromedome Flake";
   inputs = {
     # NixOS official package source, using the nixos-25.05 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
@@ -13,6 +14,7 @@ description = "Chromedome Flake";
       modules = [
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
+        nixos-hardware.nixosModules.framework-13-inch-7040-amd
         ./configuration.nix
       ];
     };
