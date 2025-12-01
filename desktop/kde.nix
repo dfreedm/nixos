f62ac6bdc2ba356bd488f06f20b6e8a03449ab6d
Daniel Freedman <dfreedm2@gmail.com>
2025-11-30 23:06:45 -0800

    remove x11

--- desktop/kde.nix
+++ desktop/kde.nix
@@ -1,9 +1,6 @@
 { pkgs, ... }:
 {
   hardware.graphics.enable = true;
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
