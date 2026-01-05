d2efbe6cd39493557931f1b633e962b55cc4582d
Daniel Freedman <dfreedm2@gmail.com>
2026-01-04 17:41:08 -0800

    enable wifi with sixer

--- hosts/sixer/default.nix
+++ hosts/sixer/default.nix
@@ -1,12 +1,13 @@
 { config, pkgs, ... }:
 {
   imports = [
-    (import ../server)
+    (import ../../server)
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
 
   networking.hostName = "sixer";
+  networking.networkmanager.enable = true;
 
   # Enable System76 kernel modules, power daemon, and firmware daemon
   hardware.system76.enableAll = true;
