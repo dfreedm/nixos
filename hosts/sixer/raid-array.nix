6ff6898016f4e94ac04db93bb776bdb303e6bb52
Daniel Freedman <dfreedm2@gmail.com>
2026-03-08 15:05:32 -0700

    remove old array

--- hosts/sixer/raid-array.nix
+++ hosts/sixer/raid-array.nix
@@ -10,25 +10,6 @@
     fileSystems = [ "/mnt/data" ];
   };
 
-  # Enable MDADM
-  boot.swraid = {
-    enable = true;
-    mdadmConf = ''
-      ARRAY /dev/md/nas  metadata=1.2 UUID=c885239d:c517b217:6a68428c:da2acecc name=pumpkin:nas
-    '';
-  };
-
-  fileSystems."/mnt/nas" = {
-    device = "/dev/md127";
-    options = [
-      "defaults"
-      # Wait 3 seconds for the disk to show up. Default is 90 seconds
-      "x-systemd.device-timeout=3s"
-      # Disk array has a separate power system, so don't block boot if it isn't on.
-      "nofail"
-    ];
-  };
-
   fileSystems."/mnt/data" = {
     device = "UUID=52558741-b3aa-45e7-a25c-77a5dd2416a0";
     options = [
